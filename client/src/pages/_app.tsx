3f53e5d13d34f70f46de59aca0087aaa32fb3823
Gabriel Santos <mfkrgabriel@gmail.com>
2021-03-31 09:47:17 -0300

    Updating favico.ico

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -8,8 +8,8 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Messaging Chat</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/img/chat-icon.ico" />
+        <link rel="apple-touch-icon" href="/img/chat-icon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
