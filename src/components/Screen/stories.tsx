e01ad25299001c8ca01430ff776778d0665c0d5c
Gabriel Santos <mfkrgabriel@gmail.com>
2021-04-01 16:42:07 -0300

    updating Stories

--- src/components/Screen/stories.tsx
+++ src/components/Screen/stories.tsx
@@ -7,17 +7,16 @@ export default {
   component: Screen
 } as Meta
 
-const id = v4()
 const messages = [
-  { message: 'Hello there!', id, user: 'Gabriel' },
+  { message: 'Hello there!', user: 'Gabriel' },
   { message: 'Hello!', id: 'id-2', user: 'Bot-1567' },
-  { message: 'How are you doing today?', id, user: 'Gabriel' },
+  { message: 'How are you doing today?', user: 'Gabriel' },
   {
     message: `I'm really good! and what about you?`,
     id: 'id-2',
     user: 'Bot-1567'
   },
-  { message: `I'm great, thanks!`, id, user: 'Gabriel' },
+  { message: `I'm great, thanks!`, user: 'Gabriel' },
   {
     message: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.`,
     id: 'id-2',
@@ -33,6 +32,6 @@ export const Default: Story<ScreenProps> = () => (
 
 export const WithMessages: Story<ScreenProps> = () => (
   <div style={{ maxWidth: '35em', margin: '0 auto', height: '100vh' }}>
-    <Screen userId={id} messages={messages} />
+    <Screen nickname={'Gabriel'} messages={messages} />
   </div>
 )
